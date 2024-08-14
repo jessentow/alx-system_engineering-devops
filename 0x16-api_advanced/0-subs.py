ed836bac0f103dafc3b305c329329557f5979a15
Jesse Ntow <Jessentow@gmail.com>
2024-08-13 21:38:49 -0700

    Advanced API

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This will query the Reddit API and returns the number
+of subscribers (not active users, total subscribers)
+for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """returns number of total subscribers"""
+    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    headers = {'User-Agent': 'CustomClient/1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return (0)
+    response = response.json()
+    if 'data' in response:
+        return (response.get('data').get('subscribers'))
+
+    else:
+        return (0)
