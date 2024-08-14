4cd5f9bb0a068d4ccd43f41a9ab5397db97bd6db
Jesse Ntow <Jessentow@gmail.com>
2024-08-13 22:39:38 -0700

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-This will query the Reddit API and returns the number
+This script queries the Reddit API and returns the number
 of subscribers (not active users, total subscribers)
 for a given subreddit.
 """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns number of total subscribers"""
-    url = ("https://api.reddit.com/r/{}/about".format(subreddit))
+    """Returns the number of total subscribers"""
+    url = f"https://api.reddit.com/r/{subreddit}/about"
     headers = {'User-Agent': 'CustomClient/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
-    else:
-        return (0)
+        return 0
+                                    
+    response_data = response.json()
+    return response_data.get('data', {}).get('subscribers', 0)
