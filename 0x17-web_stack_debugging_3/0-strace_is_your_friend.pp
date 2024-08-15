ed06e4f94b108eae1fe7b76fd412e330aa4c6b86
Jesse Ntow <Jessentow@gmail.com>
2024-08-14 20:54:33 -0700

    debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# Apache returns 500; use this script to fix typo in config
+# Apache returns 500; this script will fix typo in config
 
 exec { 'fix config typo':
   command => "sed -i 's/.phpp/.php/' /var/www/html/wp-settings.php",
